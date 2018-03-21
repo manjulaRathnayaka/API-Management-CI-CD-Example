cc6b77a51595acbdf1a07181bdc4816f8c462e89
Manjula Rathnayaka <manjula.cse@gmail.com>
2018-03-21 23:08:14 +0530

    fix deploy issue

--- DeployBackendService/deploy.sh
+++ DeployBackendService/deploy.sh
@@ -2,17 +2,22 @@
 set -e
 environment=$1
 source "$environment"Environment/backendService.properites 
-echo "login to integration cloud"
-curl -v -c /tmp/cookies -X POST -k "$SERVER_URL/appmgt/site/blocks/user/login/ajax/login.jag" -d "action=login&userName=$clouduser&password=$clouduserpass"
 
-echo "deploy micro service into integration cloud"
-appHashId=$(curl -b /tmp/cookies -X POST "$SERVER_URL/appmgt/site/blocks/application/application.jag" -d "action=getApplication&applicationName=$APPLICATION_NAME" |jq -r .hashId)
+echo "login to integration cloud"
+curl -c /tmp/cookie -X POST -k "$SERVER_URL/appmgt/site/blocks/user/login/ajax/login.jag" -d "action=login&userName=$clouduser&password=$clouduserpass"
+
+appStatusCode=$(curl -w "%{http_code}" -b /tmp/cookie -X POST "$SERVER_URL/appmgt/site/blocks/application/application.jag" -d "action=getApplication&applicationName=$APPLICATION_NAME")
+if [[ $appStatusCode == 200 ]]
+then
+appHashId=$(curl -b /tmp/cookie -X POST "$SERVER_URL/appmgt/site/blocks/application/application.jag" -d "action=getApplication&applicationName=$APPLICATION_NAME" |jq -r .hashId)
 
 if [[ ! -z $appHashId ]] 
 then
- curl -b cookies -X POST $SERVER_URL/appmgt/site/blocks/application/application.jag -F action=deleteApplication -F applicationKey=$appHashId
+ curl -b cookie -X POST $SERVER_URL/appmgt/site/blocks/application/application.jag -F action=deleteApplication -F applicationKey=$appHashId
+fi
 fi
 
-curl -b cookies -X POST $SERVER_URL/appmgt/site/blocks/application/application.jag -F action=createApplication -F applicationName=$APPLICATION_NAME -F applicationDescription=$APPLICATION_NAME -F runtime=26 -F appTypeName=mss -F applicationRevision=$APPLICATION_VERSION -F uploadedFileName=$DEPLOYABLE_ARTIFACT_NAME -F runtimeProperties=[] -F tags=[] -F fileupload=@$DEPLOYABLE_ARTIFACT_PATH/$DEPLOYABLE_ARTIFACT_NAME -F isFileAttached=true -F conSpec=2 -F isNewVersion=false -F appCreationMethod=default -F setDefaultVersion=true
+echo "deploy backend service to integration cloud"
+curl -b cookie -X POST $SERVER_URL/appmgt/site/blocks/application/application.jag -F action=createApplication -F applicationName=$APPLICATION_NAME -F applicationDescription=$APPLICATION_NAME -F runtime=26 -F appTypeName=mss -F applicationRevision=$APPLICATION_VERSION -F uploadedFileName=$DEPLOYABLE_ARTIFACT_NAME -F runtimeProperties=[] -F tags=[] -F fileupload=@$DEPLOYABLE_ARTIFACT_PATH/$DEPLOYABLE_ARTIFACT_NAME -F isFileAttached=true -F conSpec=2 -F isNewVersion=false -F appCreationMethod=default -F setDefaultVersion=true
 
 echo "application was successfully deployed"
