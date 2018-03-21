f5cf0f7f5375ec38e53d6ad0c718cd5eaf8783af
Manjula Rathnayaka <manjula.cse@gmail.com>
2018-03-21 11:36:29 +0530

    fix path

--- DeployBackendService/deploy.sh
+++ DeployBackendService/deploy.sh
@@ -2,12 +2,11 @@
 set -e
 environment=$1
 source "$environment"Environment/backendService.properites 
-
 echo "login to integration cloud"
-curl -c cookies -X POST -k "$SERVER_URL/appmgt/site/blocks/user/login/ajax/login.jag" -d "action=login&userName=$clouduser&password=$clouduserpass"
+curl -v -c /tmp/cookies -X POST -k "$SERVER_URL/appmgt/site/blocks/user/login/ajax/login.jag" -d "action=login&userName=$clouduser&password=$clouduserpass"
 
 echo "deploy micro service into integration cloud"
-appHashId=$(curl -b cookies -X POST $SERVER_URL/appmgt/site/blocks/application/application.jag -F action=getApplication -F applicationName=$APPLICATION_NAME |jq -r .hashId)
+appHashId=$(curl -b /tmp/cookies -X POST "$SERVER_URL/appmgt/site/blocks/application/application.jag" -d "action=getApplication&applicationName=$APPLICATION_NAME" |jq -r .hashId)
 
 if [[ ! -z $appHashId ]] 
 then
